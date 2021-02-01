46144d5f677f1cae57e57df9a8c03170207e1b89
OGyu kwon <haze5959@gmail.com>
2021-02-01 12:21:25 +0900

    1231313

--- main.ts
+++ main.ts
@@ -25,9 +25,9 @@ router
   });
 
 const app = new Application();
+app.use(oakCors({ origin: "*" }));
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.use(oakCors({ origin: "*" }));
 
 console.info("CAMP_MIDDLEWARE Start!!");
 
