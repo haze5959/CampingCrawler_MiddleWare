20ff40e74673547cab773c638d5c4dfec94953a5
OGyu kwon <haze5959@gmail.com>
2021-04-06 14:33:27 +0900

    2222

--- smtpClient.ts
+++ smtpClient.ts
@@ -2,20 +2,23 @@ import "https://deno.land/x/dotenv/load.ts";
 import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
 
 const client = new SmtpClient();
-await client.connectTLS({
-  hostname: "smtp.gmail.com",
-  port: 465,
-  username: Deno.env.get("GMAIL_USERNAME"),
-  password: Deno.env.get("GMAIL_PASSWORD"),
-});
 
 let mailerObj = async (title: string, body: string) => {
+  await client.connectTLS({
+    hostname: "smtp.gmail.com",
+    port: 465,
+    username: Deno.env.get("GMAIL_USERNAME"),
+    password: Deno.env.get("GMAIL_PASSWORD"),
+  });
+  
   await client.send({
     from: "oqkwon92@gmail.com",
     to: "oqkwon92@gmail.com",
     subject: title,
     content: body,
   });
+
+  await client.close();
 };
 
 export { mailerObj };
